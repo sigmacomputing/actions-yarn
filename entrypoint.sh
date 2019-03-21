1c59159bc1a4397241a383a8ca582008657b3dc9
Donald Huang <don.hcd@gmail.com>
2019-03-20 18:46:05 -0700

    echo NPM_AUTH_TOKEN length (#1)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 if [ -n "$NPM_AUTH_TOKEN" ]; then
+  echo "NPM_AUTH_TOKEN is set! length=$(echo -n "$NPM_AUTH_TOKEN" | wc -c)"
   # Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
   NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG-"$HOME/.npmrc"}"
   NPM_REGISTRY_URL="${NPM_REGISTRY_URL-registry.npmjs.org}"
@@ -10,6 +11,8 @@ if [ -n "$NPM_AUTH_TOKEN" ]; then
   # Allow registry.npmjs.org to be overridden with an environment variable
   printf "//%s/:_authToken=%s\\nregistry=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "$NPM_REGISTRY_URL" > "$NPM_CONFIG_USERCONFIG"
   chmod 0600 "$NPM_CONFIG_USERCONFIG"
+else
+  echo "NPM_AUTH_TOKEN is not set"
 fi
 
 sh -c "yarn $*"
