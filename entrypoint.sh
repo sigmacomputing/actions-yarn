30cf4fa08478be9848ed8003150831708ab3cc7a
Donald Huang <don@sigmacomputing.io>
2019-03-21 20:32:49 -0700

    one more try

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,8 @@ set -e
 
 if [ -n "$NPM_AUTH_TOKEN" ]; then
   echo "NPM_AUTH_TOKEN is set! length=$(echo -n "$NPM_AUTH_TOKEN" | wc -c)"
-  # Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
-  NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG-"$HOME/.npmrc"}"
-  NPM_REGISTRY_URL="${NPM_REGISTRY_URL-registry.npmjs.org}"
-
-  # Allow registry.npmjs.org to be overridden with an environment variable
-  printf "//%s/:_authToken=%s\\nregistry=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "$NPM_REGISTRY_URL" > "$NPM_CONFIG_USERCONFIG"
-  chmod 0600 "$NPM_CONFIG_USERCONFIG"
+  # https://github.com/johno/actions-yarn/blob/4bcd1fdf1590659b08e024e673a67d1cd13a601b/entrypoint.sh#L6
+  echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
 else
   echo "NPM_AUTH_TOKEN is not set"
 fi
