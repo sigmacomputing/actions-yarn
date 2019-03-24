bd1d56fe20fe90cf46e06adef49c25fe01887376
Kenneth Truong <kenneth@sigmacomputing.io>
2019-03-23 23:47:18 -0700

    Update entrypoint.sh
    Remove Echo

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@
 set -e
 
 if [ -n "$NPM_AUTH_TOKEN" ]; then
-  echo "NPM_AUTH_TOKEN is set! length=$(echo -n "$NPM_AUTH_TOKEN" | wc -c)"
   # https://github.com/johno/actions-yarn/blob/4bcd1fdf1590659b08e024e673a67d1cd13a601b/entrypoint.sh#L6
   echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
 else
